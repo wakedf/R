26e8df925cc22523946c8cf654450ac326cac55e
wakedf <62493928+wakedf@users.noreply.github.com>
2020-04-14 22:32:49 +0700

    Add files via upload

---
+++ type_symbol/4/Script.js
@@ -0,0 +1,17 @@
+let sym = Symbol();
+let arr = [1, 2, 3, 4]
+
+arr[sym] = function() {
+	let sum = 0;
+	for (let key of this){
+		sum+= key;
+	}
+	document.write('Сумма: ' + sum);
+}	
+
+
+
+for (let key of arr){
+	console.log(key);
+}
+arr[sym]();
